cc979867afc5793ca9d90cdb9587f58aa50e0edd
Roselene Gabun <roselenegabun@gmail.com>
2022-12-02 12:48:53 -0800

    Square of input

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,8 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int first = Integer.valueOf(scanner.nextLine());
+        // Prints the square of the output
+        System.out.println(first*first);
     }
 }
