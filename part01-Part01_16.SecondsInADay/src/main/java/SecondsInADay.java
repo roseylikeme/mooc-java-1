d4ff78df6e242b9a1a2c6721077b2a5dca9b11e1
Roselene Gabun <roselenegabun@gmail.com>
2022-12-02 08:37:42 -0800

    Convert Days to Seconds

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -7,6 +7,10 @@ public class SecondsInADay {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        int days = Integer.valueOf(scanner.nextLine());
+        int seconds = days * 86400;
+        System.out.println(seconds);
 
     }
 }
