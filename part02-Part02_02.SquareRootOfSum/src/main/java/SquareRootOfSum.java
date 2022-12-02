9fd26764bb1594db2ecf6710af50119ba0caf7e0
Roselene Gabun <roselenegabun@gmail.com>
2022-12-02 13:07:48 -0800

    Squareroot the sum of two inputs

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,17 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        //Write a program that reads two integers from the user and prints
+        //the square root of the sum of these integers. 
+        //The program does not need to work with negative values.
+        
+        // Grab two ints
+        int first = Integer.valueOf(scanner.nextLine());
+        int second = Integer.valueOf(scanner.nextLine());
+        // Sqrt of two ints
+        double squareRoot = Math.sqrt(first + second);
+        // Print out 
+        System.out.println(squareRoot);
     }
 }
