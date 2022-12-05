2016693b9720b0c49309cf3d027d5f90dd3a6082
Roselene Gabun <roselenegabun@gmail.com>
2022-12-04 23:22:30 -0800

    Count how much numbers given

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,18 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int count = 0; // tracks how much numbers given
+        while (true){
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            
+            if (num == 0){
+                break; // if num = 0; break out of loop and count how much num given
+            } else {
+                count = count + 1; // if a num other than 0 given, increase num count
+            }
+        }
+        System.out.println("Number of numbers: " + count);
     }
 }
