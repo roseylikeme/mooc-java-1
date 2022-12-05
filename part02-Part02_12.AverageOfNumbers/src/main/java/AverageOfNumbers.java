6fbf9ef9e24b63a3f055782b521610d4a08250eb
Roselene Gabun <roselenegabun@gmail.com>
2022-12-05 01:04:45 -0800

    Avg of all num inputs

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,19 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int count = 0;
+        int sum = 0;
+        while (true){
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == 0){
+                break;
+            }
+            count = count + 1;
+            sum = num + sum;
+        }
+        double avg = 1.0 * sum / count;
+        System.out.println("Average of the numbers: " + avg);
     }
 }
