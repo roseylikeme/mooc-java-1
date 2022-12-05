4770cf926987508bf6ae550147918ce49ce179b7
Roselene Gabun <roselenegabun@gmail.com>
2022-12-05 00:05:47 -0800

    Count num&sum of nums

--- part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -5,6 +5,20 @@ public class NumberAndSumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int count = 0; //keeps track of numbers inputted
+        int sum = 0; // keeps track of sum        
+        
+        while (true){
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == 0){
+                break;
+            } 
+            count = count + 1;
+            sum = sum + num;
+        }
+        System.out.println("Number of numbers: " + count);
+        System.out.println("Sum of the numbers: " + sum);
     }
 }
