07880212d6dfacebd754da23f3e4449921e498db
Roselene Gabun <roselenegabun@gmail.com>
2022-12-04 23:05:05 -0800

    Loop true till 4 is inputted

--- part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -6,5 +6,12 @@ public class AreWeThereYet {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true){
+            System.out.println("Give a number:");
+            String input = scanner.nextLine();
+            if (input.equals("4")){
+                break;
+            }
+        }
     }
 }
