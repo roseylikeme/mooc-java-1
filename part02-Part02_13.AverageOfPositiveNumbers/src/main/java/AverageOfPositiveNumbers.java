cd0417eada57f344a10c691869d61f1336ab9ee8
Roselene Gabun <roselenegabun@gmail.com>
2022-12-05 01:11:56 -0800

    Get avg of positive num

--- part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,25 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int count = 0;
+        int sum = 0;
+        double avg = 0;
+        while (true){
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == 0){
+                break;
+            }
+            if (num > 0){
+                count = count + 1; // adds up how much numbers input
+                sum = sum + num; // adds up numbers total
+            }
+        }
+        if (count == 0){
+            System.out.println("Cannot calculate the average");
+        } else{
+            avg = 1.0 * sum / count;
+            System.out.println(avg);
+        }
     }
 }
