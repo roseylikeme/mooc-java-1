6d03d4a05fc92ad4923fa76704c9e0f60b37fc4a
Roselene Gabun <roselenegabun@gmail.com>
2022-12-06 03:28:57 -0800

    From input to 100

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,11 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        // From input to 100
+        
+        int num = Integer.valueOf(scanner.nextLine());
+        for (int i = num; i < 100+1; i++){
+            System.out.println(i);
+        }
     }
 }
