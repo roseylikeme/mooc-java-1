df79250d1f36ebe99b46fb7a6a0231b5b3d5a0f4
Roselene Gabun <roselenegabun@gmail.com>
2022-12-06 05:20:12 -0800

    Repeat, break, remember complete

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -13,6 +13,34 @@ public class RepeatingBreakingAndRemembering {
         // the parts you haven't done, but you'll get points for the finished parts.
         
         Scanner scanner = new Scanner(System.in);
-
+        
+        int sum = 0;
+        int count = 0;
+        double avg = 0;
+        int even = 0;
+        int odd = 0;
+        // Keeps asking for numbers until number -1 is given
+        System.out.println("Give numbers");
+        while (true){
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == -1){
+                break;
+            } else if (num%2 == 0){
+                even += 1;
+            } else{
+                odd += 1;
+            }
+            sum += num; // totals up each num input
+            count += 1; // counts how much numbers were given
+            
+            
+        }
+        avg = 1.0*sum/count;
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + count);
+        System.out.println("Average: " + avg);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
     }
 }
