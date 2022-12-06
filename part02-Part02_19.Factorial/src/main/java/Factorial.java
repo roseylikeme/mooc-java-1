5f174f1c810a039178c1efc179d8e2087dcba4d9
Roselene Gabun <roselenegabun@gmail.com>
2022-12-06 04:00:41 -0800

    Calculate factorial from given input

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,13 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int fact = 1;
+        System.out.println("Give a number: ");
+        int num = Integer.valueOf(scanner.nextLine());
+        for (int i=1; i <= num; i++){
+            fact = fact*i;
+        }
+        System.out.println("Factorial: " + fact);
     }
 }
