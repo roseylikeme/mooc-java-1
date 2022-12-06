c6419e108dda28838792cf7b8105b14b5d9a6279
Roselene Gabun <roselenegabun@gmail.com>
2022-12-06 03:35:24 -0800

    From one input to another

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -7,5 +7,12 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Where to?");
+        int num = Integer.valueOf(scanner.nextLine());
+        System.out.println("Where from?");
+        int b = Integer.valueOf(scanner.nextLine());
+        for (int i=b; i <= num; i++){
+            System.out.println(i);
+        }
     }
 }
