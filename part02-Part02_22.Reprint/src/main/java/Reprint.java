1e68b9b8a691a1656b36eb94fe1c8da4fe12edc4
Roselene Gabun <roselenegabun@gmail.com>
2022-12-06 05:30:58 -0800

    How many times to print?

--- part02-Part02_22.Reprint/src/main/java/Reprint.java
+++ part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -8,11 +8,18 @@ public class Reprint {
         // then call the printText-method multiple times with a while-loop
         
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("How many times?"); // should the text be printed?
+        int num = Integer.valueOf(scanner.nextLine());
+        for (int i=0; i<num; i++){
+            printText();
+        }
     }
     
     // Don't change the next line that defines the method!
     // (We aren't giving a method a parameter yet)
     public static void printText() {
         // write some code here
+        System.out.println("In a hole in the ground there lived a method");
     }
 }
