976c73f2516a9e037eb37a915dd1cb17a192c879
Roselene Gabun <roselenegabun@gmail.com>
2022-12-06 03:41:38 -0800

    Sum of a sequence

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -5,6 +5,13 @@ public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int sum = 0;
+        System.out.println("Last number?");
+        int num = Integer.valueOf(scanner.nextLine());
+        for (int i = 1; i<=num; i++){
+            sum += i;
+        }
+        System.out.println("The sum is " + sum);
     }
 }
